6565eca93c28b2a1adf29561e0c4dccea16c517e
atdaniellimon <look@daniellimon.uk>
2026-09-11 12:50:42 -0600

    yet another fix

--- mac/NewtonMac/Views/Auth/AuthView.swift
+++ mac/NewtonMac/Views/Auth/AuthView.swift
@@ -67,11 +67,6 @@ public struct AuthView: View {
                         Text("Singularity")
                             .font(.system(size: 14, weight: .medium))
                             .foregroundStyle(NewtonTheme.sand.opacity(0.8))
-                        #if os(macOS)
-                        if #available(macOS 13.0, *) {
-                            .tracking(3)
-                        }
-                        #endif
                     }
                 }
                 .padding(.bottom, 50)
